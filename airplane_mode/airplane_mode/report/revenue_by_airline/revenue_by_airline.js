0f7626034c632e2c7ba3d774f82d62f066847856
itgeek <nidhin.regency@gmail.com>
2025-03-21 22:35:09 +0300

    created the report and set the user rights

--- airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.js
+++ airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Revenue by Airline"] = {
-	"filters": [
+	"filters": [{
+    fieldname: "airline",
+    label: "Airline",
+    fieldtype: "Link",
+	options: "Airline",
+	on_change: function(query_report){
+		query_report.refresh();
+	}
+    
+},
+]
 
-	]
 };
--- airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
+++ airplane_mode/airplane_mode/report/revenue_by_airline/revenue_by_airline.py
@@ -2,17 +2,18 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.query_builder.functions import Count,Sum
+from frappe.query_builder.functions import Sum,Date
 
 def execute(filters=None):
 	columns = get_columns()
-	data = get_data(filters)
-	chart = get_chart()
+	airline = filters.airline
+	end_date = filters.get("end_date")
+
+	data = get_data(airline)
+	chart = get_chart(data)
 	labels = [d["airline"] for d in data]
 	values = [d["total_amount"] for d in data]
-	report_summary = [
-		
-]
+	report_summary = get_summary(data)
 	
 	return columns, data, None, chart, report_summary
 
@@ -23,51 +24,51 @@ def get_columns():
 		{
 			"label": "Airline",
 			"fieldname": "airline",
-			"fieldtype": "Data",
+			"fieldtype": "Link",
+			"options":"Airline"
 		},
 		{
 			"label": "Revenue",
 			"fieldname": "total_amount",
-			"fieldtype": "Float",
+			"fieldtype": "Currency",
 		
-		}
+		},
+		
 	]
 	return columns
 
-def get_data(filters=None):
-	data = []
-	AirplaneTicket = frappe.qb.DocType("Airplane Ticket")
-	AirplaneFlight = frappe.qb.DocType("Airplane Flight")
-	Airplane = frappe.qb.DocType("Airplane")
-	
-	total_revenue = Sum(AirplaneTicket.total_amount).as_("total_amount")
+import frappe
+from frappe.query_builder.functions import Sum, Date
 
-	query = ( frappe.qb.from_(AirplaneTicket)
-		  .join(AirplaneFlight)
-		  .on(AirplaneFlight.name == AirplaneTicket.flight)
-		  .join(Airplane)
-		  .on(Airplane.name == AirplaneFlight.airplane)
-		  .select(
-			  Airplane.airline,
-			  #AirplaneTicket.total_amount
-			  total_revenue
-		).groupby(Airplane.airline)
-		)
+def get_data(airline):
+    AirplaneTicket = frappe.qb.DocType("Airplane Ticket") 
+    AirplaneFlight = frappe.qb.DocType("Airplane Flight")
+    Airplane = frappe.qb.DocType("Airplane")
 
-	tickets = query.run(as_dict=True)
-	for ticket in tickets:
-		row = {
-			"airline":ticket.airline,
-			"total_amount":ticket.total_amount
-		}
-		data.append(row)
-	
+    # Extract only the date part from the timestamp
+    date_only = Date(AirplaneTicket.creation).as_("date")
 
-	return data
+    total_revenue = Sum(AirplaneTicket.total_amount).as_("total_amount")
+
+    query = (
+        frappe.qb.from_(AirplaneTicket)
+        .join(AirplaneFlight).on(AirplaneFlight.name == AirplaneTicket.flight)
+        .join(Airplane).on(Airplane.name == AirplaneFlight.airplane)
+        .select(
+            #date_only,  # Group by date only, ignoring time
+            Airplane.airline,
+            total_revenue  # Summing the total_amount
+        )
+		.where(Airplane.airline == airline)#(date_only >= start_date) & (date_only <= end_date))
+    	.groupby(Airplane.airline)
+    )
+
+    tickets = query.run(as_dict=True)
+    
+    return tickets
+
 	
-	
-def get_chart():
-	data = get_data(filters=None)
+def get_chart(data):
 	labels = [d["airline"] for d in data]
 	values = [d["total_amount"] for d in data]
 	
@@ -77,11 +78,21 @@ def get_chart():
         "datasets": [
             {
                 "name": "Revenue",
-                "values": values
+                "values": values,
+				"fieldtype": "Currency",
             }
         ]
     },
-    "type": "bar"  # Chart type: bar, line, pie, etc.
+    "type": "pie"  # Chart type: bar, line, pie, etc.
 }
 	return chart
 
+def get_summary(data):
+	report_summary = []
+	for d in data:
+		row = {
+			"label": d.airline,
+			"value":d.total_amount
+		}
+		report_summary.append(row)
+	return report_summary
